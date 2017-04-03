447a690f85f5efb6cdea40be355b1ea17ca3978d
Jag <sarai.jagvir@gmail.com>
2017-04-03 16:35:48 -0700

    dom assignement complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,14 @@
 
-var animatePoints = function(){
+var revealPoint = function(){
     var points = document.getElementsByClassName("point");
-
-    var revealFirstPoint = function(){
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "sacleX(1) tranlateY(0)";
-        points[0].style.WebkitTransform = "scalex(1) translateY(0)";
-    };
-
-    var revealSecondPoint = function(){
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "sacleX(1) tranlateY(0)";
-        points[1].style.WebkitTransform = "scalex(1) translateY(0)";
-    };
-
-    var revealThirdPoint = function(){
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "sacleX(1) tranlateY(0)";
-        points[2].style.WebkitTransform = "scalex(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    
+    for(var i=0; i<points.length; i++){
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "sacleX(1) tranlateY(0)";
+        points[i].style.WebkitTransform = "scalex(1) translateY(0)";
+    }
+  
 };
                 
                  
